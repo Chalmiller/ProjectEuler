37357d4306baf22341463b16a80e30781537de16
Chris Miller <christopher@transcriptic.com>
2019-04-14 19:45:20 -0700

    Largest Prime factor

--- Solutions/largest_prime_factor.py
+++ Solutions/largest_prime_factor.py
@@ -5,24 +5,26 @@ What is the largest prime factor of the number 600851475143 ?
 """
 
 def isPrime(x):
-    for i in range(2,x-1):
+    for i in range(2,x/2):
         if (x%i == 0):
             return False
     else:
         return True
 
 def largestPrimeFactor():
+    divisors = []
     prime_factors = []
     magic_number = 600851475143
 
     for magic_num_factors in range(5, magic_number/2):
-        if (magic_number%magic_num_factors == 0 and magic_num_factors%2 != 0
-                and magic_num_factors%3 != 0 and magic_num_factors%5 != 0):
-            if (isPrime(magic_num_factors)):
-                prime_factors.append(magic_num_factors)
-                print(magic_num_factors)
+        if (magic_number%magic_num_factors == 0):
+            divisors.append(magic_num_factors)
         else:
             continue
+
+    for factors in divisors:
+        if (isPrime(factors)):
+            prime_factors.append(factors)
 
     return prime_factors[-1]
 
